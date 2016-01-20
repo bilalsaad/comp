7bc1c70cc15cedc996d4ed164bc1106a3c96e702
bilalsaad <abusaadb@post.bgu.ac.il>
2016-01-20 22:00:29 +0200

    not working

--- CMP/FINALPROJ/compiler.ml
+++ CMP/FINALPROJ/compiler.ml
@@ -317,13 +317,14 @@ let nt_sexpr =
      let nt_sexpr = delayed make in
      let nt_dot = add_skip nt_sexpr (char '.') in
      let nt_proper_list = caten nt_lb (caten (star nt_sexpr) nt_rb) in
-     let rec proper_to_pair = function
+     let rec proper_to_pair = (function
        | [] -> Nil
-       | x::xs -> Pair(x,proper_to_pair xs) in
-     let rec improper_to_pair = function
+       | x::xs ->  Pair(x,proper_to_pair xs)) in
+     
+     let rec improper_to_pair = (function
        | [] | [_] -> raise X_this_should_not_happen
        | x :: y :: [] -> Pair(x,y) 
-       | x::xs -> Pair(x, improper_to_pair xs) in
+       | x::xs -> Pair(x, improper_to_pair xs)) in
      let nt_proper_list = pack nt_proper_list (fun (_,(ls,_)) -> ls) in
      let nt_proper_list = pack nt_proper_list proper_to_pair in
      let nt_improper_list = caten nt_lb (plus nt_sexpr) in
